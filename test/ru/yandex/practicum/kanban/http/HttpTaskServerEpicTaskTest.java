9ca4d93668df15c915f8396afa8ee610708bcdd0
Dmitriy Skoropad <skoropad.ds@yandex.ru>
2024-12-20 08:34:38 +0500

    test(http): add an abstract class for tests
    Sprint 9 Review 2

--- test/ru/yandex/practicum/kanban/http/HttpTaskServerEpicTaskTest.java
+++ test/ru/yandex/practicum/kanban/http/HttpTaskServerEpicTaskTest.java
@@ -1,20 +1,14 @@
 package ru.yandex.practicum.kanban.http;
 
-import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.kanban.manager.InMemoryTaskManager;
-import ru.yandex.practicum.kanban.manager.TaskManager;
 import ru.yandex.practicum.kanban.task.EpicTask;
 import ru.yandex.practicum.kanban.task.StatusTask;
 import ru.yandex.practicum.kanban.task.SubTask;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -23,15 +17,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HttpTaskServerEpicTaskTest {
+class HttpTaskServerEpicTaskTest extends HttpTaskServerTest {
 
-    static final String URI_FIRST = "http://localhost:8080";
-    final TaskManager manager = new InMemoryTaskManager();
-    final HttpTaskServer taskServer = new HttpTaskServer(manager);
-    final Gson gson = HttpTaskServer.getGson();
+    EpicTask epicTask;
 
-
-    HttpTaskServerEpicTaskTest() throws IOException {
+    protected HttpTaskServerEpicTaskTest() throws IOException {
     }
 
     static class EpicListTypeToken extends TypeToken<List<EpicTask>> {
@@ -41,50 +31,39 @@ class HttpTaskServerEpicTaskTest {
     }
 
     @BeforeEach
-    void setUp() {
-        manager.clearTaskGroup();
-        manager.clearSubGroup();
-        manager.clearEpicGroup();
-        taskServer.start();
+    public void setUp() throws IOException, InterruptedException {
+        super.setUp();
+        epicTask = new EpicTask("EpicTask 1", "Description 1");
+        manager.addTask(epicTask);
+        endPoint = "/epics";
     }
 
     @AfterEach
-    void tearDown() {
-        taskServer.stop();
+    public void tearDown() {
+        super.tearDown();
     }
-
-    // Тесты /epics
 
     // Должен быть 200: OK при запросе списка эпиков
     @Test
     void shouldBeSuccessForGetEpics() throws IOException, InterruptedException {
-        final EpicTask epic = new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>());
-        manager.addTask(epic);
-        HttpResponse<String> response = sendGet("/epics");
-
+        HttpResponse<String> response = sendGet(endPoint);
         assertEquals(200, response.statusCode());
 
         List<EpicTask> epics = gson.fromJson(response.body(), new EpicListTypeToken().getType());
-
         assertNotNull(epics);
         assertEquals(1, epics.size());
-        assertEquals(epic, epics.getFirst());
+        assertEquals(epicTask, epics.getFirst());
     }
 
     // Должен быть 200: OK при запросе списка подзадач эпика
     @Test
     void shouldBeSuccessForGetEpicSubtasks() throws IOException, InterruptedException {
-        manager.addTask(new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>()));
         manager.addTask(new SubTask(1, "Subtask 1", "Description 1"));
         manager.addTask(new SubTask(1, "Subtask 2", "Description 2"));
-        HttpResponse<String> response = sendGet("/epics/1/subtasks");
-
+        HttpResponse<String> response = sendGet(endPoint + "/1/subtasks");
         assertEquals(200, response.statusCode());
 
         List<SubTask> subTasks = gson.fromJson(response.body(), new SubtaskListTypeToken().getType());
-
         assertNotNull(subTasks);
         assertEquals(2, subTasks.size());
         assertEquals("Subtask 1", subTasks.getFirst().getTitle());
@@ -93,36 +72,25 @@ class HttpTaskServerEpicTaskTest {
     // Должен быть 200: OK при запросе существующего эпика по ID
     @Test
     void shouldBeSuccessForGetEpicById() throws IOException, InterruptedException {
-        final EpicTask epic = new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>());
-        manager.addTask(epic);
-        HttpResponse<String> response = sendGet("/epics/1");
-
+        HttpResponse<String> response = sendGet(endPoint + "/1");
         assertEquals(200, response.statusCode());
 
         EpicTask epicJson = gson.fromJson(response.body(), EpicTask.class);
-
         assertNotNull(epicJson);
-        assertEquals(epic, epicJson);
+        assertEquals(epicTask, epicJson);
     }
 
     // Должен быть 404: Not Found при запросе НЕ существующего эпика по ID
     @Test
     void shouldBeNotFoundForGetEpicById() throws IOException, InterruptedException {
-        manager.addTask(new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>()));
-        HttpResponse<String> response = sendGet("/epics/10");
-
+        HttpResponse<String> response = sendGet(endPoint + "/10");
         assertEquals(404, response.statusCode());
     }
 
     // Должен быть 200: OK при удалении существующего эпика по ID
     @Test
     void shouldBeSuccessForDeleteEpicById() throws IOException, InterruptedException {
-        manager.addTask(new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>()));
-        HttpResponse<String> response = sendDelete("/epics/1");
-
+        HttpResponse<String> response = sendDelete(endPoint + "/1");
         assertEquals(200, response.statusCode());
         assertTrue(manager.getEpicGroup().isEmpty());
     }
@@ -130,45 +98,36 @@ class HttpTaskServerEpicTaskTest {
     // Должен быть 404: OK при удалении НЕ существующего эпика по ID
     @Test
     void shouldBeNotFoundForDeleteEpicById() throws IOException, InterruptedException {
-        manager.addTask(new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>()));
-        HttpResponse<String> response = sendDelete("/epics/10");
-
+        HttpResponse<String> response = sendDelete(endPoint + "/10");
         assertEquals(404, response.statusCode());
     }
 
     // Должно быть 200: OK при добавлении эпика
     @Test
     void shouldBeSuccessForAddEpic() throws IOException, InterruptedException {
-        final EpicTask epic = new EpicTask("Epic 1", "Description 1", 0, StatusTask.NEW,
+        manager.clearEpicGroup();
+        epicTask = new EpicTask("EpicTask 1", "Description 1", 0, StatusTask.NEW,
                 Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>());
-        String epicJson = gson.toJson(epic);
-        HttpResponse<String> response = sendPost("/epics", epicJson);
-
+        String epicJson = gson.toJson(epicTask);
+        HttpResponse<String> response = sendPost(endPoint, epicJson);
         assertEquals(201, response.statusCode());
 
         List<EpicTask> epics = manager.getEpicGroup();
-
         assertNotNull(epics);
         assertEquals(1, epics.size());
-        assertEquals("Epic 1", epics.getFirst().getTitle());
+        assertEquals("EpicTask 1", epics.getFirst().getTitle());
     }
 
     // Должно быть 200: OK при обновлении существующего эпика
     @Test
     void shouldBeSuccessForUpdateEpic() throws IOException, InterruptedException {
-        manager.addTask(new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>()));
-
         EpicTask updateEpic = new EpicTask("Updated Epic 1", "Description 1", 1, StatusTask.NEW,
                 Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>());
         String epicJson = gson.toJson(updateEpic);
-        HttpResponse<String> response = sendPost("/epics", epicJson);
-
+        HttpResponse<String> response = sendPost(endPoint, epicJson);
         assertEquals(201, response.statusCode());
 
         List<EpicTask> epics = manager.getEpicGroup();
-
         assertNotNull(epics);
         assertEquals(1, epics.size());
         assertEquals("Updated Epic 1", epics.getFirst().getTitle());
@@ -177,46 +136,15 @@ class HttpTaskServerEpicTaskTest {
     // Должно быть 404: Not Found при обновлении НЕ существующего эпика
     @Test
     void shouldBeNotFoundForUpdateEpic() throws IOException, InterruptedException {
-        manager.addTask(new EpicTask("Epic 1", "Description 1", 1, StatusTask.NEW,
-                Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>()));
-
         EpicTask updateEpic = new EpicTask("Updated Epic 1", "Description 1", 10, StatusTask.NEW,
                 Duration.ofMinutes(15), LocalDateTime.now(), new ArrayList<>());
         String epicJson = gson.toJson(updateEpic);
-        HttpResponse<String> response = sendPost("/epics", epicJson);
-
+        HttpResponse<String> response = sendPost(endPoint, epicJson);
         assertEquals(404, response.statusCode());
 
         List<EpicTask> epics = manager.getEpicGroup();
-
         assertNotNull(epics);
         assertEquals(1, epics.size());
-        assertEquals("Epic 1", epics.getFirst().getTitle());
-    }
-
-    // Вспомогательные методы
-
-    HttpResponse<String> sendGet(String path) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URI_FIRST + path))
-                .GET()
-                .build();
-        return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    HttpResponse<String> sendDelete(String path) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URI_FIRST + path))
-                .DELETE()
-                .build();
-        return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    HttpResponse<String> sendPost(String path, String text) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URI_FIRST + path))
-                .POST(HttpRequest.BodyPublishers.ofString(text))
-                .build();
-        return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals("EpicTask 1", epics.getFirst().getTitle());
     }
 }
